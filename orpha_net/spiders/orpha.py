bf32d42f3700cfacf9cb096936a4a4262fe51c56
Ming Jing <jingming.cn@gmail.com>
2025-06-06 18:43:02 +0800

    -

--- orpha_net/spiders/orpha.py
+++ orpha_net/spiders/orpha.py
@@ -15,7 +15,6 @@ class OrphaSpider(scrapy.Spider):
         with open('OrphaCode.csv', 'r') as file:
             lines = file.readlines()
             for orphacode in lines[1:]:
-                print(orphacode)
                 yield scrapy.Request(url=base_url+orphacode+"?name="+orphacode+"&mode=orpha",callback=self.parse)
 
     def parse(self, response):
